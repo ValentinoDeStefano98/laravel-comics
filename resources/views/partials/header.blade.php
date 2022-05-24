c400ea54f74c1f82e4a9df12739b84eac96a32d8
ValentinoDeStefano98 <valentino.destefano46@gmail.com>
2022-05-24 12:25:31 +0200

    Header fixed

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,41 +1,48 @@
 <header>
-        <div class="header-container">
-            <img src="{{ asset('images/dc-logo.png') }}" alt="Logo" class="logo">
-            <ul class="ul-header">
-                <li class="header-li">
-                    <a href="{{ route('home') }}">Characters</a>
-                </li>
-                <li class="header-li">
-                    <a href="{{ route('comics') }}">Comics</a>
-                </li>
-                <li class="header-li">
-                    <a href="#">Movies</a>
-                </li>
-                <li class="header-li">
-                    <a href="#">TV</a>
-                </li>
-                <li class="header-li">
-                    <a href="#">Games</a>
-                </li>
-                <li class="header-li">
-                    <a href="#">Collectibles</a>
-                </li>
-                <li class="header-li">
-                    <a href="#">Videos</a>
-                </li>
-                <li class="header-li">
-                    <a href="#">Fans</a>
-                </li>
-                <li class="header-li">
-                    <a href="#">News</a>
-                </li>
-                <li class="header-li">
-                    <a href="#">Shop</a>
-                </li>
-            </ul> 
+    <div class="pre-header">
+        <div class="pre-header-container resize">
+            <div class="pre-header-elm">DC Power Visa</div>
+            <div class="pre-header-elm">Addictional DC Sites <i class="fa-solid fa-angle-down"></i></div>
+        </div>       
+    </div>
+    <div class="header-container resize">
+        <img src="{{ asset('images/dc-logo.png') }}" alt="Logo" class="logo">
+        <ul class="ul-header">
+            <li class="header-li">
+                <a href="{{ route('home') }}">Characters</a>
+            </li>
+            <li class="header-li">
+                <a href="{{ route('comics') }}">Comics</a>
+            </li>
+            <li class="header-li">
+                <a href="#">Movies</a>
+            </li>
+            <li class="header-li">
+                <a href="#">TV</a>
+            </li>
+            <li class="header-li">
+                <a href="#">Games</a>
+            </li>
+            <li class="header-li">
+                <a href="#">Collectibles</a>
+            </li>
+            <li class="header-li">
+                <a href="#">Videos</a>
+            </li>
+            <li class="header-li">
+                <a href="#">Fans</a>
+            </li>
+            <li class="header-li">
+                <a href="#">News</a>
+            </li>
+            <li class="header-li">
+                <a href="#">Shop <i class="fa-solid fa-angle-down"></i></a>
+            </li>
+        </ul> 
+        <div class="search-container">
+            Search <br> <i class="fa-solid fa-magnifying-glass"></i>
         </div>
-        <div class="jumbotron">
-
-        </div>
-        
-    </header>
+    </div>
+    <div class="jumbotron">
+    </div>
+</header>
